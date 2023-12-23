33f6c16dbadd25295e6cf89e331039a5e468203f
Connor Stevens <conmail1224@gmail.com>
2023-12-22 21:58:34 -0500

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import { PostHog } from './posthog';
 
+export const viewport: Viewport = {
+	themeColor: '#171717',
+};
+
 export const metadata: Metadata = {
 	title: 'RouteFinder',
+	applicationName:
+		'RouteFinder — Find real-world routes to fly in your simulator',
+	description: `We've curated over 15,000 real-world routes flown by dozens of airlines. Figuring out where to fly shouldn't be a chore. Get started today.`,
+	keywords: ['simulator', 'x-plane', 'msfs', 'flight sim', 'flight simulator'],
+	openGraph: {
+		title: 'RouteFinder',
+		siteName: 'RouteFinder',
+		description: `We've curated over 15,000 real-world routes flown by dozens of airlines. Figuring out where to fly shouldn't be a chore. Get started today.`,
+		url: 'https://routes.cnrstvns.dev',
+		images: ['/og.png'],
+	},
+	metadataBase:
+		process.env.NODE_ENV === 'production'
+			? new URL('https://routes.cnrstvns.dev')
+			: new URL('http://localhost:3000'),
 };
 
 type LayoutProps = {
